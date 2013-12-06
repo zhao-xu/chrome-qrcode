7094deaa59dad1788eac45c2c2f89cdda8c9e3a9
zhaoxu <zhaoxu.email@gmail.com>
2013-12-06 23:17:26 +0800

    Add qrcode image decoding
    + decode qrcode via context menu
    + add icons

---
+++ js/background.js
@@ -0,0 +1,23 @@
+qrcode.callback = function(text) {
+    if (text == 'error decoding QR Code') {
+        alert('无法解析二维码图片')
+    } else {
+        alert(text);
+    }
+};
+
+function onClickHandler(info, tab) {
+    if (info.menuItemId == 'decode') {
+        qrcode.decode(info.srcUrl);
+    }
+}
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+        id:'decode',
+        title:chrome.i18n.getMessage('decode'),
+        contexts:['image']
+    })
+});
